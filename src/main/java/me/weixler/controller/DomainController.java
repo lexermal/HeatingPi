395c65d38ea665982b8663090d9947792f8775f5
mmobil <mmobil@local>
2018-11-06 22:35:36 +0100

    (Domain)Added redirect for suburls

--- src/main/java/me/weixler/controller/DomainController.java
+++ src/main/java/me/weixler/controller/DomainController.java
@@ -1,32 +1,24 @@
 package me.weixler.controller;
 
-import me.weixler.beans.Pin;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class DomainController {
 
-
-//    @GetMapping("**")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        List<Pin> pins=new ArrayList<>();
-        model.addAttribute("leds", pins);
-        return "index";
-    }
-
     @GetMapping("/schema")
-    public String upload(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String schema() {
         return "redirect";
     }
 
+    @GetMapping("/pins")
+    public String pins() {
+        return "redirect";
+    }
 
+    @GetMapping("/active")
+    public String active() {
+        return "redirect";
+    }
 
 }
