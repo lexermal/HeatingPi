a2cec61e9a0a8665646095407e5a398c879092a8
Manuel <manuel@weixler.me>
2019-01-05 12:36:33 +0100

    [Misc](Build)Updated build parameters

--- build-project.sh
+++ build-project.sh
@@ -6,4 +6,4 @@ cd ../../../
 rm -Rf src/main/resources/static/
 cp src/main/frontend/build/ src/main/resources/static -r
 ./gradlew build
-java -jar build/libs/Heizungspi-1.0-SNAPSHOT.jar
+java -jar build/libs/HeizungsPi-1.0-SNAPSHOT.jar
