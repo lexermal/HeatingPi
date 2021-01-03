c20a3dabaaaa82f7bfa42b5a06ea919243e2cac5
lexermal <contact@weixler.me>
2021-01-03 20:21:08 +0100

    [FRONTEND](project)Moved frontend

--- v2/build-project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "starting building project"
-cd src/main/frontend/ || exit
-yarn build
-cd ../../../
-rm -Rf src/main/resources/static/
-cp src/main/frontend/build/ src/main/resources/static -r
-./gradlew build
-# java -jar build/libs/HeizungsPi-1.0-SNAPSHOT.jar
